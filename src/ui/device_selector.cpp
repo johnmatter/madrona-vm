06325594d7e1dc834024525fa51fe2d0e6b50a6d
John Matter <jmatter4@gmail.com>
2025-06-20 02:20:47 -0400

    fix device selector
    https://arthursonzogni.com/FTXUI/doc/examples_2component_2gallery_8cpp-example.html

--- src/ui/device_selector.cpp
+++ src/ui/device_selector.cpp
@@ -1,16 +1,30 @@
 #include "ui/device_selector.h"
 #include "audio/device_info.h"
 #include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
 #include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
 #include <iostream>
 namespace ui {
+using namespace ftxui;
+  // Display a component nicely with a title on the left.
+  Component Wrap(std::string name, Component component) {
+    return Renderer(component, [name, component] {
+      return hbox({
+        text(name) | size(WIDTH, EQUAL, 8),
+        separator(),
+        component->Render() | xflex,
+      }) |
+      xflex;
+    });
+  }
 unsigned int DeviceSelector::selectAudioDevice() {
-  using namespace ftxui;
   auto devices = AudioDeviceManager::getAvailableDevices();
   if (devices.empty()) {
     std::cout << "No audio devices found." << std::endl;
     return 0;
   }
+  // get device names and identify default device
   std::vector<std::string> device_names;
   unsigned int default_device_idx = 0;
   for (size_t i = 0; i < devices.size(); ++i) {
@@ -21,34 +35,28 @@ unsigned int DeviceSelector::selectAudioDevice() {
     }
     device_names.push_back(name);
   }
-  int selected = static_cast<int>(default_device_idx);
-  unsigned int selected_id = 0;
-  auto radiobox = Radiobox(&device_names, &selected);
-  radiobox |= Renderer([&](Element inner) {
-    return vbox({
-               text(L"Select an audio output device:"),
-               separator(),
-               inner,
-               separator(),
-               text(L"Press Enter to confirm or Escape to quit."),
-           }) |
-           border;
-  });
+  // https://arthursonzogni.com/FTXUI/doc/examples_2component_2gallery_8cpp-example.html
+  int selected = 0;
   auto screen = ScreenInteractive::TerminalOutput();
-  auto renderer = Renderer(radiobox, [&] {
-    return vbox({
-        radiobox->Render(),
-    });
+  auto radiobox = Radiobox(&device_names, &selected);
+  radiobox = Wrap("Select a device", radiobox);
+  std::string button_label = "Accept";
+  std::function<void()> on_button_clicked_;
+  auto accept = Button(&button_label, screen.ExitLoopClosure());
+  accept = Wrap("", accept);
+  auto layout = Container::Vertical({
+      radiobox,
+      accept,
   });
-  auto component = CatchEvent(renderer, [&](Event event) {
-    if (event == Event::Return) {
-      selected_id = devices[selected].id;
-      screen.Exit();
-      return true;
-    }
-    return false;
+  auto component = Renderer(layout, [&] {
+    return vbox({
+      radiobox->Render(),
+      separator(),
+      accept->Render(),
+    }) |
+    xflex | size(WIDTH, GREATER_THAN, 40) | border;
   });
   screen.Loop(component);
-  return selected_id;
+  return devices[selected].id;
 }
-} // namespace ui 
+} // namespace ui
